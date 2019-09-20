0890e778d09ff994db72565951aad63fe84d6e7c
LarisaLG <LarisaLG@users.noreply.github.com>
2019-09-20 11:26:48 +0100

    add some small features

--- shared.js
+++ shared.js
@@ -7,16 +7,20 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener("click", function() {
       // modal.style.display = "block";
-      // backdrop.style.display = "block";
       // modal.className = 'open'  //this will actually overwrite the complete class list
       modal.classList.add('open');
-      backdrop.classList.add('open');
+      backdrop.style.display = "block";
+      // backdrop.classList.add('open');
+      setTimeout(function(){
+        backdrop.classList.add('open');
+      }, 10);
     });
   }
   
@@ -37,12 +41,31 @@ if(modalNoButton) {
    if (modal) {
      modal.classList.remove('open');
    }
+    //backdrop.style.display = "none";
     backdrop.classList.remove('open');
+    setTimeout(function(){
+      backdrop.classList.remove('none');
+    }, 200);
   }
 
   toggleButton.addEventListener('click', function(){
     // mobileNav.style.display = 'block';
-    // backdrop.style.display = "block";
+    
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = "block";
+    setTimeout(function(){
+      backdrop.classList.add('open');
+    }, 10);
+    
   });
+
+  ctaButton.addEventListener('animationstart', function(event){
+    console.log('Animation started', event);
+  })
+
+  ctaButton.addEventListener('animationend', function(event){
+    console.log('Animation ended', event);
+  })
+  ctaButton.addEventListener('animationiteration', function(event){
+    console.log('Animation iteration', event);
+  })
